cb40d66f4d957a697b7637b7a3172038638690d0
bachproef <maxdekoninck@Maxs-MacBook-Pro.local>
2013-11-27 19:53:46 +0100

    Een nieuwe stress test toegevoegd
    das 300 keer met 500 verschillende sets 100000 operaties doen

--- source/tests.cpp
+++ source/tests.cpp
@@ -91,36 +91,86 @@ void test_smallbasicsetstress() {
                 break;
             case 2:
                 cout << s[irand2].toString() << " intersect " << s[irand3].toString() << " = " << (s[irand2].setintersect(s[irand3])).toString() << endl;
-                ;
                 break;
-            //case 3:
-            //    cout << s[irand2].toString() << " / " << s[irand3].toString() << " = " << (s[irand2].setdifference(s[irand3])).toString() << endl;
-            //    break;
+                //case 3:
+                //    cout << s[irand2].toString() << " / " << s[irand3].toString() << " = " << (s[irand2].setdifference(s[irand3])).toString() << endl;
+                //    break;
             case 3:
                 cout << s[irand2].toString() << " subset " << s[irand3].toString() << " = " << (s[irand2].hasAsSubset(s[irand3])) << endl;
                 break;
         }
     }
-
+    
     clock_t end = clock();
     
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     cout << "-End timing-" << endl;
     cout << "# TESTS - ran in " << elapsed_secs << " sec #"<< endl;
 }
+    
+    
+
+double speed_test_smallbasicsetstress() {
+    int aantaloperaties=100000;
+    SmallBasicSet s[500];
+    cout << "# TESTS - STRESS TEST SMALL BASIC SETS #" << endl;
+    cout << "-Start timing-" << endl;
+    clock_t begin = clock();
+    //maken van  small sets
+    for (int i=1; i<=500; i++) {
+        SmallBasicSet si = SmallBasicSet(i);
+        s[i]= si;
+    }
+    //uitvoeren van  operaties op random sets
+    for (int j = 0; j<aantaloperaties; j++) {
+        int irand = rand() % 3 + 1;
+        int irand2 = rand() % 500 + 0;
+        int irand3 = rand() % 500 + 0;
+        switch ( irand )
+        {
+            case 1:
+                s[irand2].setunion(s[irand3]);
+                break;
+            case 2:
+                s[irand2].setintersect(s[irand3]);
+                break;
+            //case 3:
+            //    cout << s[irand2].toString() << " / " << s[irand3].toString() << " = " << (s[irand2].setdifference(s[irand3])).toString() << endl;
+            //    break;
+            case 3:
+                s[irand2].hasAsSubset(s[irand3]);
+                break;
+        }
+    }
+
+    clock_t end = clock();
+    
+    double elapsed_secs = double (end - begin) / (CLOCKS_PER_SEC / 1000);
+    cout << "-End timing-" << endl;
+    cout << "# TESTS - ran in " << elapsed_secs << " msec #"<< endl;
+    return elapsed_secs;
+}
 /**
  * Testmodule om een beetje in te experimenteren
  */
-void test_general() {
-	// do some stuff
+void test_speed() {
+    
+    double gem = 0;
+    for(int i = 300 ; i > 0 ; i--){
+        gem = gem + speed_test_smallbasicsetstress();
+    }
+    cout << "gem runtime is: " << gem/300 << "ms" << endl;
+    
+    
 }
 
 int main() {
 	// run the tests
-	test_smallbasicset();
-	test_amfunction();
-	test_general();
+	//test_smallbasicset();
+	//test_amfunction();
+	//test_general();
     //test_smallbasicsetint();
-    test_smallbasicsetstress();
+    //test_smallbasicsetstress();
+    test_speed();
 	return 0;
 }
