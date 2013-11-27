52fba9cc597354259ec62859829f0b6ec855836f
bachproef <maxdekoninck@Maxs-MacBook-Pro.local>
2013-11-27 17:33:58 +0100

    test toegevoegd

---
+++ Test.cpp
@@ -0,0 +1,9 @@
+//
+//  Test.cpp
+//  AMFBach
+//
+//  Created by Max Dekoninck on 27/11/13.
+//  Copyright (c) 2013 Daan en Max. All rights reserved.
+//
+
+#include "Test.h"
---
+++ Test.h
@@ -0,0 +1,14 @@
+//
+//  Test.h
+//  AMFBach
+//
+//  Created by Max Dekoninck on 27/11/13.
+//  Copyright (c) 2013 Daan en Max. All rights reserved.
+//
+
+#ifndef __AMFBach__Test__
+#define __AMFBach__Test__
+
+#include <iostream>
+
+#endif /* defined(__AMFBach__Test__) */
