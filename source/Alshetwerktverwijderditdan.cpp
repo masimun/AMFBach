088d401c972e0eeae2aee45f940e1ef1f4550cb4
bachproef <maxdekoninck@Maxs-MacBook-Pro.local>
2013-11-27 18:38:19 +0100

    werkt het verwijder dan dat keer

---
+++ source/Alshetwerktverwijderditdan.cpp
@@ -0,0 +1,9 @@
+//
+//  Alshetwerktverwijderditdan.cpp
+//  AMFBach
+//
+//  Created by Max Dekoninck on 27/11/13.
+//  Copyright (c) 2013 Daan en Max. All rights reserved.
+//
+
+#include "Alshetwerktverwijderditdan.h"
