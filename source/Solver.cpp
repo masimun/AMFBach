d694619394891ec88bdd0623ca5dd36f2b653e50
Daan Seynaeve <daanseynaeve2@hotmail.com>
2014-03-27 00:53:12 +0100

    .

--- source/Solver.cpp
+++ source/Solver.cpp
@@ -204,6 +204,7 @@ long long Solver::PatricksCoefficient(AMFunction r1, AMFunction r2) {
 
 
 
+
 /*
 void Solver::verynaivededekind() {
 	int const n = 4; // works instant up to 4... and 5 takes a while.
